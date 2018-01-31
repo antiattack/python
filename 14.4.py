1872713877ba2aa1d5092fac9d4f929849ebd495
destroyeru573 <35898952+destroyeru573@users.noreply.github.com>
2018-02-01 00:04:53 +0330

    Add files via upload

---
+++ 14.4.py
@@ -0,0 +1,9 @@
+print("HI")
+
+list=[]
+for i in range(0,5,+1):
+    x=float(input("enter you average: ".title()))
+    list.append(x)
+    list.sort()
+#print(list)
+print("Second place: ",list[3])
