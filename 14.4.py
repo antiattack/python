70d1785000aac9d54142a571d4010ebd27447e89
destroyeru573 <35898952+destroyeru573@users.noreply.github.com>
2018-01-31 20:45:45 +0330

    Delete 14.4.py

--- 14.4.py
+++
@@ -1,9 +0,0 @@
-print("HI")
-
-list=[]
-for i in range(0,5,+1):
-    x=float(input("enter you average: ".title()))
-    list.append(x)
-    list.sort()
-#print(list)
-print("Second place: ",list[3])
