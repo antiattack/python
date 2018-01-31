0fbd01de37e132a4bc6711a95ab75a57bc745b17
destroyeru573 <35898952+destroyeru573@users.noreply.github.com>
2018-01-31 20:41:03 +0330

    Add files via upload

---
+++ 14.2.py
@@ -0,0 +1,18 @@
+#exercise ( sum(), append(), list[], sort() )
+print("HI")
+b=0
+list=[]
+
+for i in range(0,5,+1):
+    x=int(input("Enter Number: "))
+    if (x<0):
+        list.append(x)
+        f=list
+        b=b+1
+        f.sort()
+print("Number of negative numbers: ",b)
+print("negative numbers are: ",f)
+
+
+sum=sum(f)
+print("sumation: ",sum)
