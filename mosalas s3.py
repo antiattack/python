95ec2096076ba5961899027504b65c4c4bb2bdeb
destroyeru573 <35898952+destroyeru573@users.noreply.github.com>
2018-02-01 00:09:31 +0330

    Add files via upload

---
+++ mosalas s3.py
@@ -0,0 +1,10 @@
+print("hi")
+print()
+#n=int(input("enter row: ").title())
+m=int(input("enter clomn: ").title())
+#print("*"*m,)
+
+for i in range(0,m,+1):
+    print("*"*m)
+    print("\ ")
+    m=m-1
